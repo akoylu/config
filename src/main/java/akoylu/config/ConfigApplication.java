12d6e51eb4fd9539379ed4fd0f4009f4a4b36bae
Ahmet Köylü <akoylu@gmail.com>
2021-01-26 18:45:57 +0300

    Eureka server

--- src/main/java/akoylu/config/ConfigApplication.java
+++ src/main/java/akoylu/config/ConfigApplication.java
@@ -2,8 +2,14 @@ package akoylu.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaServer
+@EnableDiscoveryClient
 public class ConfigApplication {
 
 	public static void main(String[] args) {
